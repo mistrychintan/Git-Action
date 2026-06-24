146bc1e7904500eadacea0726669dce3fadb1f86
Chintan Mistry <48353167+mistrychintan@users.noreply.github.com>
2026-06-24 09:36:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 procider "aws" {
   region ="us-east-2"
 }
-resource "aws_s3_bucket" "example" {
+resource "aws_instance" "GitAction_Demo" {
+  ami           = "ami-0e5497a77ef21b5ac"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "GitAction-Demo"
+  }
+}
+resource "aws_s3_bucket" "GitAction_Demo" {
   bucket = "my-tf-test-bucket"
 
   tags = {
